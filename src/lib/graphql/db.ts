e983c07e13dd029374a0f819bc971335cde63955
Daniel Maros <marosdaniel@gmail.com>
2025-10-02 11:22:41 +0200

    fix prod deploy2

--- src/lib/graphql/db.ts
+++ src/lib/graphql/db.ts
@@ -1,8 +1,8 @@
-import clientPromise from '../mongodb';
+import getClient from '../mongodb';
 
 const DB_NAME = process.env.MONGODB_DB ?? 'cookbook';
 
 export const getDb = async () => {
-  const client = await clientPromise;
-  return client.db(DB_NAME);
+  const clientPromise = await getClient();
+  return clientPromise.db(DB_NAME);
 };
--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -3,25 +3,30 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI as string;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Please add your Mongo URI to .env.local');
 }
 
-if (process.env.NODE_ENV === 'development') {
-  // @ts-expect-error Global augmentation for MongoDB client promise in development
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
+export const getClient = async (): Promise<MongoClient> => {
+  if (process.env.NODE_ENV === 'development') {
     // @ts-expect-error Global augmentation for MongoDB client promise in development
-    global._mongoClientPromise = client.connect();
+    if (!global._mongoClientPromise) {
+      client = new MongoClient(uri, options);
+      // @ts-expect-error Global augmentation for MongoDB client promise in development
+      global._mongoClientPromise = client.connect();
+    }
+    // @ts-expect-error Global augmentation for MongoDB client promise in development
+    clientPromise = global._mongoClientPromise;
+  } else {
+    if (!clientPromise) {
+      client = new MongoClient(uri, options);
+      clientPromise = client.connect();
+    }
   }
-  // @ts-expect-error Global augmentation for MongoDB client promise in development
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+  return clientPromise;
+};
 
-export default clientPromise;
+export default getClient;
