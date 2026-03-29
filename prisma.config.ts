97eb5bf7ab8095ef8fe697c36b4c4a57060e5ff6
Daniel Maros <marosdaniel@gmail.com>
2026-03-29 12:45:29 +0200

    refactor: move database connection configuration from prisma.config.ts to schema.prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,3 @@
 import { defineConfig } from '@prisma/config';
 
-export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
-  },
-});
+export default defineConfig({});
