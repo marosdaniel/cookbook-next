05cdd5b104f22aa47fe1dacacd06885a69587884
Daniel Maros <marosdaniel@gmail.com>
2026-04-07 09:41:49 +0200

    refactor: convert test utility functions and components to arrow function syntax

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -2,16 +2,18 @@ import { MantineProvider } from '@mantine/core';
 import { type RenderOptions, render } from '@testing-library/react';
 import type { ReactElement, ReactNode } from 'react';
 
-export function MockProviders({ children }: Readonly<{ children: ReactNode }>) {
+export const MockProviders = ({
+  children,
+}: Readonly<{ children: ReactNode }>) => {
   return <MantineProvider>{children}</MantineProvider>;
-}
+};
 
-function customRender(
+const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, 'wrapper'>,
-) {
+) => {
   return render(ui, { wrapper: MockProviders, ...options });
-}
+};
 
 export * from '@testing-library/react';
 export { customRender as render };
