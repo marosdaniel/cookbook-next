0f58d67fb30d38f80583881be15574629a5adb6f
Daniel Maros <marosdaniel@gmail.com>
2026-07-23 09:33:34 +0200

    feat: new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,11 @@ export async function generateMetadata(): Promise<Metadata> {
     manifest: '/site.webmanifest',
     icons: {
       icon: [
+        {
+          url: '/favicon-transparent.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
         { url: '/favicon.ico', sizes: 'any' },
         { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
         { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
